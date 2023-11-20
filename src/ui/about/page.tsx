0c2428f038eacd7a96966ab07dc2f3a2e1559c4b
Tanner Hornsby <thornsby@uchicago.edu>
2023-11-19 20:16:47 -0600

    adding ui folders for routes

---
+++ src/ui/about/page.tsx
@@ -0,0 +1,13 @@
+// create a basic about page
+import React from 'react';
+
+
+export default function About() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-6xl font-bold text-primary-olivine">
+            About
+        </h1>
+        </div>
+    )
+    }
---
+++ src/ui/market/page.tsx
@@ -0,0 +1,13 @@
+// create a basic market page
+import React from 'react';
+
+
+export default function Market() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-6xl font-bold text-primary-olivine">
+            Market
+        </h1>
+        </div>
+    )
+}
