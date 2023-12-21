14d59659af0a9c3a7423f6786b8bbb1b825adb66
Tanner Hornsby <thornsby@uchicago.edu>
2023-12-20 18:48:38 -0600

    fixing env

--- src/lib/steam-config.js
+++ src/lib/steam-config.js
@@ -4,10 +4,14 @@ require('dotenv').config();
 const apiKey = process.env.STEAM_API_KEY;
 let host = process.env.HOST;
 
+
 // if we are in dev mode, use localhost
 if (process.env.NODE_ENV === 'development') {
     host = 'http://localhost:3000';
 }
+
+console.log('process.env is ', process.env);
+console.log('host is', host);
 
 const config = {
     returnURL: host + '/validate',
